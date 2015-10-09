416650772df9e10225c70a962b09b79e55ea67d7
HU Pili <hpl1989@gmail.com>
2015-10-09 16:32:01 +0800

    done the draft

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,16 @@
  * Controller of the initernalsApp
  */
 angular.module('initernalsApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope, $sce) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+  $scope.changePrefix = function(){
+  	  var url = "https://www.google.com/calendar/embed?src=" + this.prefix + "%40theinitium.com&ctz=Asia/Hong_Kong"
+  	  $scope.url = $sce.trustAsResourceUrl(url)
+  }
+
   });

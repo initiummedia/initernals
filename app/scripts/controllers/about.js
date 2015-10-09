3a44427623b1d0c31c0d437869237e867d197297
HU Pili <hpl1989@gmail.com>
2015-10-09 16:19:00 +0800

    init initernals

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name initernalsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the initernalsApp
+ */
+angular.module('initernalsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

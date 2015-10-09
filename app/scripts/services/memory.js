ca9ca902d5936d132799051c9d3d00244d143fbb
HU Pili <hpl1989@gmail.com>
2015-10-09 16:44:22 +0800

    boilerplate for service

---
+++ app/scripts/services/memory.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name initernalsApp.Memory
+ * @description
+ * # Memory
+ * Service in the initernalsApp.
+ */
+angular.module('initernalsApp')
+  .service('Memory', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
